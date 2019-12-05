f95b63e1663f5e823c574d0e299b9d89b3904e22
Alexandre Bouchard <38104156+abouchard-ds@users.noreply.github.com>
2019-12-05 15:01:34 -0500

    cheap log rotate
    5 min script, Windows 2008 R2 server

---
+++ oracle_log_rotate.cmd
@@ -0,0 +1,56 @@
+::@ECHO OFF 
+SETLOCAL ENABLEEXTENSIONS
+
+:: Alexandre Bouchard <alexandre.bouchard@bell.ca>
+:: December 2019
+:: --------------------------------------------------
+:: This script is used to rotate Oracle's logs.
+:: All parameters are kept in the file, as variables.
+
+:: TODO: learn to use windows array so we may loop all logs folders
+::       in the same code. but since the system will be decom., may not have time for that.
+
+TITLE Oracle Logs Rotate
+
+SET logdate=%date:~-4%%date:~3,2%%date:~0,2%
+SET Now=%Time: =0%
+SET logtime=%Now:~0,2%%Now:~3,2%
+
+:: ------------ listener logs 
+
+if exist blabla\listener\trace (
+	cd blabla\listener\trace
+) else (
+	echo 'Folder does not exists.'
+	exit
+)
+
+if exist listener.log (
+	move listener.log listener_%logdate%-%logtime%.log
+	zip -q -m "listener_%logdate%-%logtime%.zip" "listener_%logdate%-%logtime%.log"
+) else (
+	echo 'Logfile not found.'
+	exit
+)
+
+ForFiles /p blabla\listener\trace /s /d -30 /c "cmd /c del @file"
+
+:: ------------ database logs 
+if exist blabla\trace (
+	cd blabla\trace
+) else (
+	echo 'Folder does not exists.'
+	exit
+)
+
+if exist alert_prdsyn01.log (
+	move alert_01.log alert_01_%logdate%-%logtime%.log
+	zip -q -m "alert_01_%logdate%-%logtime%.zip" "alert_01_%logdate%-%logtime%.log"
+) else (
+	echo 'Logfile not found.'
+	exit
+)
+
+
+:: enable later, need to keep archives first
+:: ForFiles /p blabla\trace /s /d -365 /c "cmd /c del @file"
